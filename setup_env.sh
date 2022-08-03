5b2a24c21fe8688136d538693bb988b7b0897447
Amit Srivastava <amit.srivastava@talkdesk.com>
2022-08-03 07:04:52 -0700

    upgrade setup for GPU VM to install tensorflow-gpu instead of tensorflow - not tested though

--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt install -y git unzip git-lfs ffmpeg sox libsoxr-dev libsox-fmt-all
 /databricks/python3/bin/pip install --upgrade pip
-/databricks/python3/bin/pip install tensorflow
+/databricks/python3/bin/pip install tensorflow-gpu
 /databricks/python3/bin/pip install numpy pandas pydub soundfile speechpy sklearn
 git clone https://github.com/td-amit/acoustic_cnn.git
 export PYTHONPATH=$PYTHONPATH:/databricks/driver/acoustic_cnn/
