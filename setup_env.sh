b147b83348bf710e98fdf1d3d287f1f5ee635ec5
Amit Srivastava <amit.srivastava@talkdesk.com>
2022-08-03 14:21:58 -0700

    adding pysox to the installation

--- setup_env.sh
+++ setup_env.sh
@@ -3,8 +3,8 @@
 apt-get update && apt install -y git unzip git-lfs ffmpeg sox libsoxr-dev libsox-fmt-all
 /databricks/python3/bin/pip install --upgrade pip
 /databricks/python3/bin/pip install tensorflow-gpu
-/databricks/python3/bin/pip install numpy pandas pydub soundfile speechpy sklearn
+/databricks/python3/bin/pip install numpy pandas pydub soundfile speechpy sklearn sox
 git clone https://github.com/td-amit/acoustic_cnn.git /tmp/acoustic_cnn
-export PYTHONPATH=$PYTHONPATH:/tmp/acoustic_cnn/
+export PYTHONPATH=$PYTHONPATH:/tmp:/tmp/acoustic_cnn/
 
 exit 0
